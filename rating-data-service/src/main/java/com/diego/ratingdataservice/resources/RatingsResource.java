170e37532828f85ea36c2ec54568f6ab87c85176
Diegod96 <ddphillyfan@gmail.com>
2020-08-12 19:47:27 -0400

    Created model for Ratings

---
+++ rating-data-service/src/main/java/com/diego/ratingdataservice/resources/RatingsResource.java
@@ -0,0 +1,18 @@
+package com.diego.ratingdataservice.resources;
+
+
+import com.diego.ratingdataservice.models.Rating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsResource {
+
+    @RequestMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
+
+}
